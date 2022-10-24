339b9842b26937b9fe27415411320b3f1b366df4
Zhou <richforest0506@gmail.com>
2022-10-24 10:11:47 +0800

    Update sort

--- sort.js
+++ sort.js
@@ -5,19 +5,12 @@ const seconds_batches = [
     [1, 2, 3, 32, 33, 34, 35, 36, 37, 38, 39, 40],
   ]
   
-let sorted_uniq_seconds = [];
-seconds_batches.forEach(element => {
-    element.forEach(item => {
-        var i;
-        for (i=0; i<sorted_uniq_seconds.length; i++) {
-            if (sorted_uniq_seconds[i] === item) break;                        
-            if (sorted_uniq_seconds[i] > item) {
-                sorted_uniq_seconds.splice(i, 0, item);
-                break;
-            }
-        }
-        if (i == sorted_uniq_seconds.length)
-            sorted_uniq_seconds.push(item);
-    });
-});
+var arr = [].concat.apply([], seconds_batches);
+arr = arr.sort(function (a, b) { return a*1 - b*1; });
+let sorted_uniq_seconds = [arr[0]];
+for (var i = 1; i < arr.length; i++) { 
+    if (arr[i-1] !== arr[i]) {
+      sorted_uniq_seconds.push(arr[i]);
+    }
+}
 console.log(sorted_uniq_seconds);
